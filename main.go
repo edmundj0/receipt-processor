3bd41d3265dc32f8debd4921dd9e31b2746dbb25
Edmund Ju <edmundju@berkeley.edu>
2023-10-07 23:16:31 -0700

    server up

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"receipt-processor/cmd/handlers"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", handlers.Home)
+	e.Logger.Fatal(e.Start(":8080"))
+}
