db41f2ea0af8eca88040413ea10b6433579360e9
Edmund Ju <edmundju@berkeley.edu>
2023-10-08 12:42:54 -0700

    fix logic for alphanumeric retailer and time calculation

--- cmd/handlers/rootHandler.go
+++ cmd/handlers/rootHandler.go
@@ -5,7 +5,8 @@ import (
 	"strconv"
 	"strings"
 	"math"
-	"time"
+	"fmt"
+	"unicode"
 
 	"github.com/labstack/echo/v4"
 	"github.com/google/uuid"
@@ -18,6 +19,7 @@ type Receipt struct {
 	PurchaseTime string `json:"purchaseTime"`
 	Total string `json:"total"`
 	Items []map[string]interface{} `json:"items"`
+	Points int `json:"points"`
 }
 
 type Response struct {
@@ -49,6 +51,10 @@ func ProcessReceipt(c echo.Context) error {
 
 	receiptID := uuid.New().String()
 	receipt.ID = receiptID
+
+	receipt.Points = calculatePoints(receipt)
+	fmt.Println(receipt)
+
 	receiptStore[receiptID] = *receipt
 
 	response := Response{ID: receiptID}
@@ -59,21 +65,30 @@ func calculatePoints(receipt *Receipt) int {
 	points := 0
 
 	// One point for every alphanumeric character in the retailer name
-	points += len(receipt.Retailer)
+	for _, char := range receipt.Retailer {
+		if unicode.IsLetter(char) || unicode.IsDigit(char) {
+			points++
+		}
+	}
+
+	fmt.Println(points,"alphanumeric")
 
 	// 50 points if the total is a round dollar amount with no cents
 	if strings.HasSuffix(receipt.Total, ".00") {
 		points += 50
+		fmt.Println("50 for no cents")
 	}
 
 	// 25 points if the total is a multiple of 0.25
 	totalFloat, _ := strconv.ParseFloat(receipt.Total, 64)
 	if math.Mod(totalFloat, 0.25) == 0 {
 		points += 25
+		fmt.Println("25 for multiple of 0.25")
 	}
 
 	// 5 points for every two items on the receipt
-	count := len(Receipt.Items)
+	count := len(receipt.Items)
+	fmt.Println("two items", (count/2) * 5)
 	points += (count / 2) * 5
 
 	// If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned
@@ -93,12 +108,16 @@ func calculatePoints(receipt *Receipt) int {
 	}
 
 	// 10 points if the time of purchase is after 2:00pm and before 4:00pm
-	purchaseTime, err := time.parse("15.04", receipt.PurchaseTime)
-	if err == nil {
-		purchaseTimeNumber := purchaseTime.Hour()*100 + purchaseTime.Minute()
-		if purchaseTimeNumber > 1400 && purchaseTimeNumber < 1600 {
-			points += 10
-		}
+	timeStr := receipt.PurchaseTime
+	timeParts := strings.Split(timeStr, ":")
+	hours, err := strconv.Atoi(timeParts[0])
+	minutes, err := strconv.Atoi(timeParts[1])
+
+	timeInt := hours * 100 + minutes
+
+	if timeInt > 1400 && timeInt < 1600 {
+		points += 10
+		fmt.Println("purchase time")
 	}
 
 	return points
